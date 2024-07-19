1121b6640034486f8ef0bf05d18a3b45747c3516
sudo-white-devil <white.devil.yare.yare@gmail.com>
2024-07-19 19:57:35 +0530

    Name and Client id , secret Change

--- config/api.config.js
+++ config/api.config.js
@@ -10,8 +10,8 @@
 module.exports = {
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
   // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
-  clientId: 'd87bcc39-1750-4ca0-ad54-f8d0efbb2735',
-  obfuscatedClientSecret: 'U2FsdGVkX1830zo3/pFDqaBCVBb37iLw3WnBDWGF9GIB2f4apzv0roemp8Y+iIxI3Ih5ecyukqELQEGzZlYiWg==',
+  clientId: '3ca5c7cc-b186-4766-a389-bc36fbaff88a',
+  obfuscatedClientSecret: 'U2FsdGVkX1+n0Cx5PMsox99pWdfljiYGlkdtFUtMox930gZcsEOWxfGpbajtF8xmDaicg7dgf927smx0tZgp/w==',
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
   // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
